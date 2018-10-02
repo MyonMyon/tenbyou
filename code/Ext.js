4605577f37e226dbd94ed4af9616cf7ea7cb4739
Myon2.0 <myon@myon>
2018-10-02 16:35:05 +0300

    Added toTitleCase to String functions

--- code/Ext.js
+++ code/Ext.js
@@ -25,3 +25,18 @@ extend = function (child, parent) {
     }
     return child;
 };
+
+/**
+ * Capitalizes first character of the string.
+ *
+ * @return {String} New string containing processed text.
+ */
+Object.defineProperty(String.prototype, "toTitleCase", {
+    enumerable: false,
+    value: function () {
+        if (this.length === 0) {
+            return "";
+        }
+        return this[0].toUpperCase() + this.slice(1);
+    }
+});
