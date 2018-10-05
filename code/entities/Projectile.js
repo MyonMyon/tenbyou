5740d5208bda64b2b54d73d198d14dd335480937
nyasha <localhost@localhost>
2018-10-05 14:00:21 +0300

    Returned old enemy projectile destruction bound

--- code/entities/Projectile.js
+++ code/entities/Projectile.js
@@ -43,11 +43,12 @@ Projectile.prototype.draw = function (context) {
 Projectile.prototype.step = function () {
     this.$step();
 
+    var div = this.playerside ? 1.8 : 2;
     //remove from world
-    if (this.x > this.parentWorld.width / 1.8 + this.width * 2
-            || this.x < -this.parentWorld.width / 1.8 - this.width * 2
-            || this.y > this.parentWorld.height / 1.8 + this.width * 2
-            || this.y < -this.parentWorld.height / 1.8 - this.width * 2)
+    if (this.x > this.parentWorld.width / div + this.width * 2
+            || this.x < -this.parentWorld.width / div - this.width * 2
+            || this.y > this.parentWorld.height / div + this.width * 2
+            || this.y < -this.parentWorld.height / div - this.width * 2)
         this.remove();
 
     if (!this.playerside) {
