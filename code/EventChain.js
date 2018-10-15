e32b93bbfafc77d3fef44b839fe57c304a8a3cb6
Myon2.0 <myon@myon>
2018-10-15 19:33:59 +0300

    Fire event after counting

--- code/EventChain.js
+++ code/EventChain.js
@@ -31,13 +31,13 @@ EventChain.prototype.tick = function () {
     var t = this.parent.relTime();
     for (var i in this.events) {
         if (!this.events[i].done && t >= this.events[i].second) {
-            this.events[i].fire(this.parent);
             if (this.events[i].repeatsLeft) {
                 --this.events[i].repeatsLeft;
                 this.events[i].second += this.events[i].repeatInterval;
             } else {
                 this.events[i].done = true;
             }
+            this.events[i].fire(this.parent);
         }
     }
 };
