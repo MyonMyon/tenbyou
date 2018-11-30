0a757c5d825a1c9b07400dbd88c070df520ee7c1
Myon2.0 <myon@myon>
2018-11-30 17:48:11 +0200

    Revert "Some rebalance"
    This reverts commit 122181b53bb9f101fc1b3d2f3d9f1612be589a1d.

--- code/game/Spell.js
+++ code/game/Spell.js
@@ -313,17 +313,17 @@ var SPELL = {
         boss: "snoop",
         number: 420,
         names: [
-            "Weed Sign \"It is Weednesday My Dudes\"",
-            "Weed Sign \"Smoke Weed Every Day\"",
-            "Weed Sign \"Stoned\"",
-            "Weed Sign \"Stoned to the Core\""
+            "Weed Sign \"Four Twenty\"",
+            "Weed Sign \"Four Twenty Won\"",
+            "Weed Sign \"Four Twenty Too\"",
+            "Weed Sign \"Four Twenty (Free)\""
         ],
         background: {
             file: "420.jpg",
             speed: 0
         },
-        health: 400,
-        time: 49,
+        health: 800,
+        time: 29,
         decrTime: 9,
         bonus: 42000,
         bonusBound: 420,
