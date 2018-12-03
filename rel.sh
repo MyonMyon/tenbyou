b12b4efa0f32f21b1a918f3a193593711cef2ada
Myon2.0 <myon@myon>
2018-12-03 21:27:39 +0200

    Script update to include automatic version increment

--- rel.sh
+++ rel.sh
@@ -3,13 +3,21 @@ REL_DATE=$(date +%Y-%m-%d)
 REV_INNER=$(($(git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count)+1))
 REV_TOTAL=$(($(git rev-list --count HEAD)+1))
 
-sed -i.bak "s/\(ENGINE_VER = \"\).*\(\";\)/\1$1\2/g" $FILE
+if [ "$1" ]; then
+    VERSION_NEW=$1;
+else
+    VERSION_OLD=$(sed -n "s:.*ENGINE_VER = \"\(.*\)\";:\1:p" $FILE)
+    IFS=\. read -a numbers <<<"$VERSION_OLD"
+    VERSION_NEW=${numbers[0]}.${numbers[1]}.$((${numbers[2]} + 1))
+fi
+
+sed -i.bak "s/\(ENGINE_VER = \"\).*\(\";\)/\1$VERSION_NEW\2/g" $FILE
 sed -i.bak "s/\(RELEASE_DATE = \"\).*\(\";\)/\1$REL_DATE\2/g" $FILE
 sed -i.bak "s/\(REVISION_INNER = \).*\(;\)/\1$REV_INNER\2/g" $FILE
 sed -i.bak "s/\(REVISION_TOTAL = \).*\(;\)/\1$REV_TOTAL\2/g" $FILE
 rm $FILE.bak
 
-VERSION_TAG=${1,,}
+VERSION_TAG=${VERSION_NEW,,}
 VERSION_TAG=${VERSION_TAG/ /.}
 
 MESSAGE="v$VERSION_TAG
