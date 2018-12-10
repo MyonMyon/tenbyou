4021edd56fe794fb7f97d3e0fa007118273b9d28
Myon2.0 <myon@myon>
2018-12-10 18:51:37 +0200

    Bonus fix did an oopsie
    Fixed regression from 62e75df4b5c1c40193b553a226db9f6341423b63

--- engine/entities/Bonus.js
+++ engine/entities/Bonus.js
@@ -39,6 +39,7 @@ Bonus.prototype.step = function () {
     if (offScreen > 0) {
         var offSpeed = offScreen * 5; //show on screen in 0.2 seconds
         if (Math.abs(this.x1) > offSpeed) {
+            this.x1 = -this.x1;
         } else {
             this.x1 = this.x > 0 ? -offSpeed : offSpeed;
             console.log("MORE", offSpeed, this, this.world.width / 2);
