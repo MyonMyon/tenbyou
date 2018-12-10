MyonMyon/tenbyou

commit c527b32d0874b4e717d43afe690951e9adcbc8b8

Another projectile step optimization, ~10% faster now
Added collisionCheck method.

engine/World.js
@@ -182,6 +182,14 @@ World.prototype.angleBetweenEntities = function (entity1, entity2) {
     return Math.atan2(entity2.y - entity1.y, entity2.x - entity1.x);
 };
 
+World.prototype.collisionCheck = function (entity1, entity2, distance) {
+    distance = (distance || 0) + entity1.width + entity2.width;
+    if (Math.abs(entity1.x - entity2.x) > distance || Math.abs(entity1.y - entity2.y) > distance) {
+        return false;
+    }
+    return this.distanceBetweenEntities(entity1, entity2) > distance;
+};
+
 World.prototype.distanceBetweenEntities = function (entity1, entity2) {
     return Math.sqrt(Math.pow(entity1.x - entity2.x, 2) + Math.pow(entity1.y - entity2.y, 2));
 };

engine/entities/Enemy.js
@@ -142,7 +142,7 @@ Enemy.prototype.step = function () {
     //collision with placed player weapons
     for (var i in this.world.entities) {
         var w = this.world.entities[i];
-        if (w instanceof Weapon && !w.isInvulnerable() && this.world.distanceBetweenEntities(this, w) < this.width + w.width) {
+        if (w instanceof Weapon && !w.isInvulnerable() && this.world.collisionCheck(this, w)) {
             w.hit();
         }
     }
@@ -153,7 +153,7 @@ Enemy.prototype.step = function () {
         for (var i in  this.world.entities) {
             var e = this.world.entities[i];
             if (e instanceof Projectile && e.playerSide) {
-                if (this.world.distanceBetweenEntities(this, e) < (this.width + e.width)) {
+                if (this.world.collisionCheck(this, e)) {
                     this.hurt(e.damage, {x: e.x, y: e.y});
                     e.remove();
                 }

engine/entities/Player.js
@@ -174,20 +174,21 @@ Player.prototype.step = function () {
         var e = this.world.entities[i];
         if (e instanceof Projectile && !e.playerSide && e.width) {
             //collision
-            var d = this.world.distanceBetweenEntities(e, this);
-            if (d < (e.width + this.width)) {
-                e.remove();
-                if (!this.isInvulnerable()) {
-                    this.kill();
-                    break;
+            if (this.world.collisionCheck(e, this, this.grazeWidth)) {
+                if (this.world.collisionCheck(e, this)) {
+                    e.remove();
+                    if (!this.isInvulnerable()) {
+                        this.kill();
+                        break;
+                    }
+                } else if (e.grazed < e.damage && !this.isInvulnerable()) {
+                    ++this.graze;
+                    var xD = this.x - e.x;
+                    var yD = this.y - e.y;
+                    var s = new Particle(this.world, this.x, this.y, 0.25, 8, false, false, "spark");
+                    s.setVectors(null, null, xD * 5, yD * 5);
+                    ++e.grazed;
                 }
-            } else if (d < (e.width + this.grazeWidth) && e.grazed < e.damage && !this.isInvulnerable()) {
-                ++this.graze;
-                var xD = this.x - e.x;
-                var yD = this.y - e.y;
-                var s = new Particle(this.world, this.x, this.y, 0.25, 8, false, false, "spark");
-                s.setVectors(null, null, xD * 5, yD * 5);
-                ++e.grazed;
             }
         }
     }

engine/entities/Weapon.js
@@ -28,7 +28,7 @@ Weapon.prototype.step = function () {
     if (!this.isInvulnerable()) {
         for (var i in this.world.entities) {
             var e = this.world.entities[i];
-            if (e instanceof Projectile && !e.playerSide && e.width && this.world.distanceBetweenEntities(e, this) < e.width + this.width) {
+            if (e instanceof Projectile && !e.playerSide && e.width && this.world.collisionCheck(e, this)) {
                 //collision
                 e.remove();
                 this.hit();

commit 3383309715e89de25fe583872f36e15a3860d602

Optimization did an oopsie
Fixed regression from c527b32d0874b4e717d43afe690951e9adcbc8b8

engine/World.js
@@ -187,7 +187,7 @@ World.prototype.collisionCheck = function (entity1, entity2, distance) {
     if (Math.abs(entity1.x - entity2.x) > distance || Math.abs(entity1.y - entity2.y) > distance) {
         return false;
     }
-    return this.distanceBetweenEntities(entity1, entity2) > distance;
+    return this.distanceBetweenEntities(entity1, entity2) < distance;
 };
 
 World.prototype.distanceBetweenEntities = function (entity1, entity2) {