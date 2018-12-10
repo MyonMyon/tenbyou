754c7406c345f663d86fb7986792cee1f8c3feb4
Myon2.0 <myon@myon>
2018-12-10 21:12:53 +0200

    v0.4.35
    This is an automated commit.

--- engine/Init.js
+++ engine/Init.js
@@ -1,7 +1,7 @@
-var ENGINE_VERSION = "0.4.34";
+var ENGINE_VERSION = "0.4.35";
 var RELEASE_DATE = "2018-12-10";
-var REVISION_INNER = 3;
-var REVISION_TOTAL = 484;
+var REVISION_INNER = 1;
+var REVISION_TOTAL = 485;
 
 var PRIORITY_CODE = [
     "engine/Ext",
