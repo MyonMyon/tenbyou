1a2cb859d47fba7665168f2e1343f6c93e2ef6b3
nyasha <localhost@localhost>
2018-12-15 04:19:51 +0200

    Started making charStates functionality in dialogues

--- engine/ui/Dialogue.js
+++ engine/ui/Dialogue.js
@@ -2,8 +2,19 @@ function Dialogue(world, lines) {
     world.dialogue = this;
     this.world = world;
     this.lines = lines;
-    this.time = 0;
     this.index = 0;
+    this.charStates = {};
+    this.activeChar = null;
+    for (var i in this.lines) {
+        if (this.lines[i].char && !this.charStates[this.lines[i].char]) {
+            this.charStates[this.lines[i].char] = {};
+        }
+        if (this.lines[i].position) {
+            this.charStates[this.lines[i].char].position = this.lines[i].position;
+        }
+    }
+    this.updateCharStates();
+    this.time = 0;
     this.lettersPS = 40;
 }
 
@@ -11,24 +22,29 @@ Dialogue.prototype.tick = function () {
     this.time += 1 / this.world.ticksPS;
 };
 
+Dialogue.prototype.updateCharStates = function () {
+    if (this.lines[this.index].char) {
+        this.activeChar = this.lines[this.index].char;
+        for (var i in this.charStates) {
+            this.charStates[i].active = this.activeChar === i;
+        }
+    }
+};
+
 Dialogue.prototype.next = function () {
-    var reqTime = this.lines[this.index].time || this.lines[this.index].text.length / this.lettersPS;
-    console.log(this.time, reqTime);
-    if (this.time < reqTime) {
-        this.time = reqTime;
-        return;
+    if (this.lines[this.index].text) {
+        var reqTime = this.lines[this.index].time || this.lines[this.index].text.length / this.lettersPS;
+        if (this.time < reqTime) {
+            this.time = reqTime;
+            return;
+        }
     }
     this.time = 0;
     if (++this.index >= this.lines.length) {
         this.world.dialogue = null;
         return;
     }
-    var keepProps = ["character", "position"];
-    for (var i in keepProps) {
-        if (!this.lines[this.index][keepProps[i]]) {
-            this.lines[this.index][keepProps[i]] = this.lines[this.index - 1][keepProps[i]];
-        }
-    }
+    this.updateCharStates();
 };
 
 Dialogue.prototype.draw = function () {
@@ -41,14 +57,13 @@ Dialogue.prototype.draw = function () {
             DIALOGUE_W * vp.zoom,
             DIALOGUE_H * vp.zoom);
 
-    var char = this.lines[this.index].char;
-    var charName = char;
+    var charName = this.activeChar;
     var charColor;
-    if (CHAR[char]) {
-        charName = CHAR[char].name;
-        charColor = CHAR[char].color;
+    if (CHAR[this.activeChar]) {
+        charName = CHAR[this.activeChar].name;
+        charColor = CHAR[this.activeChar].color;
     }
-    vp.context.textAlign = this.lines[this.index].position || "left";
+    vp.context.textAlign = this.charStates[this.activeChar].position || "left";
     var x = vp.context.textAlign === "left" ? DIALOGUE_X + DIALOGUE_MX : DIALOGUE_X + DIALOGUE_W - DIALOGUE_MX;
     vp.setFont(FONT.character);
     if (charColor) {
