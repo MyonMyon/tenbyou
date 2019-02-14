afb2f20c8b0517aaad6ca83959ae23d0d5b01863
Myon2.0 <myon@myon>
2019-02-14 15:31:12 +0200

    v0.5.21
    This is an automated commit.

--- engine/Init.js
+++ engine/Init.js
@@ -1,7 +1,7 @@
-var ENGINE_VERSION = "0.5.20";
-var RELEASE_DATE = "2019-02-13";
+var ENGINE_VERSION = "0.5.21";
+var RELEASE_DATE = "2019-02-14";
 var REVISION_INNER = 3;
-var REVISION_TOTAL = 611;
+var REVISION_TOTAL = 614;
 
 var PRIORITY_CODE = [
     "engine/Ext",
