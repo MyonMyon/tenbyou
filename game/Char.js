446e6cabaac201049e83cb32da24ecaaad6f7b6f
Myon2.0 <myon@myon>
2019-02-22 16:11:09 +0200

    Freyja: Special beam is not breakable

--- game/Char.js
+++ game/Char.js
@@ -39,6 +39,7 @@ var CHAR = {
         onSpecial: function () {
             this.projectileSpecial = new Beam(this.world, 0, -10, 200, Util.toAngle("s"), 0, Math.PI * (Math.random() >= 0.5 ? 2 : -2), 0, 0, 0, 10, true, "beamBlueSpecial");
             this.projectileSpecial.damagePS = 500;
+            this.projectileSpecial.breakable = false;
             this.projectileSpecial.removeTime = 1;
             this.projectileSpecial.setAnchor(this);
         },
