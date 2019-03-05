ac34b851dcccec7956e431e1539ba746152b80e3
Myon2.0 <myon@myon>
2019-03-05 17:18:17 +0200

    If negation for more readable code

--- engine/util/Sound.js
+++ engine/util/Sound.js
@@ -7,10 +7,11 @@ function Sound() {
 }
 
 Sound.play = function (data) {
-    if (Settings.get("sound.enabled")) {
-        if (!data.noSeek && !data.object.ended) {
-            data.object = new Audio(data.object.src);
-        }
-        data.object.play();
+    if (!Settings.get("sound.enabled")) {
+        return;
     }
+    if (!data.noSeek && !data.object.ended) {
+        data.object = new Audio(data.object.src);
+    }
+    data.object.play();
 };
