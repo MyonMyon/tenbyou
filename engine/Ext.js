3b29c78e74327a6e65426a4b1ace998f9bd8b4fd
Myon2.0 <myon@myon>
2019-03-21 18:00:48 +0200

    Removed old class implementation

--- engine/Ext.js
+++ engine/Ext.js
@@ -1,32 +1,4 @@
 /**
- * Implementation of OOP-like way to extend classes.
- *
- * @param {Object} child Destination for properties/methods.
- * @param {Object} parent Source of properties/methods.
- * @returns {Object} Child object. Not used.
- */
-extend = function (child, parent) {
-    for (var property in parent) {
-        if (!child[property]) {
-            child[property] = parent[property];
-        } else {
-            //super-modifier to call overrided functions
-            child["$" + property] = parent[property];
-        }
-    }
-    if (!child.classes) {
-        child.classes = [];
-    }
-    //I'm very sorry:
-    child.classes.push(child.constructor.toString().split("function ")[1].split("(")[0]);
-    //Some workaround I guess?
-    if (child.init) {
-        child.init();
-    }
-    return child;
-};
-
-/**
  * Capitalizes first character of the string.
  *
  * @return {String} New string containing processed text.
