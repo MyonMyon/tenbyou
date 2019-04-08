961a3edd62db95ecf09d4c9db91403f56a46f02f
resq <localhost@localhost.localhost>
2019-04-08 17:16:13 +0300

    v0.5.59
    This is an automated commit.

--- engine/Init.js
+++ engine/Init.js
@@ -1,7 +1,7 @@
-const ENGINE_VERSION = "0.5.58";
-const RELEASE_DATE = "2019-04-05";
-const REVISION_INNER = 6;
-const REVISION_TOTAL = 732;
+const ENGINE_VERSION = "0.5.59";
+const RELEASE_DATE = "2019-04-08";
+const REVISION_INNER = 3;
+const REVISION_TOTAL = 735;
 
 const PRIORITY_CODE = [
     "game/Values",
